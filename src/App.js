8ea31ad91aa76b364e89c0367ed882265126a218
ubaidillah <ubaidillah04@yahoo.com>
2025-05-12 16:31:19 +0700

    add module app.js, Landing_Page.js, Navbar.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
+// Import necessary modules from React library
+import React, { useEffect } from 'react';
+// Import components for routing from react-router-dom library
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// Import custom Navbar component
+import Navbar from './NavBar/Navbar';
+import Landing_Page from './Landing_Page/LandingPage';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Set up BrowserRouter for routing */}
+      <h1>Ini diatas BrowserRouter</h1>
+      <BrowserRouter>
+        {/* Display the Navbar component */}
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={<Landing_Page />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
